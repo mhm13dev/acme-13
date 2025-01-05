1f04bd2b296e4fb571b25168b5624dcdc378d324
Mubashir Hassan <mhm13dev@gmail.com>
2025-01-05 13:06:54 +0500

    feta: change localhost to 127.0.0.1

--- apps/hono-api/src/index.ts
+++ apps/hono-api/src/index.ts
@@ -3,7 +3,7 @@ import { serve } from "@hono/node-server";
 import { app } from "./app.js";
 
 const port = 5001;
-console.log(`Server is running on http://localhost:${port}`);
+console.log(`Server is running on http://127.0.0.1:${port}`);
 
 serve({
   fetch: app.fetch,
