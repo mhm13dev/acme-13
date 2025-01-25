9551743f6a02469e9133835544a8d0a778360324
Mubashir Hassan <mhm13dev@gmail.com>
2025-01-26 02:16:33 +0500

    (feat/admin): set access / refresh token cookies expiry from decoded token payload

--- apps/admin/app/(auth)/login/actions.ts
+++ apps/admin/app/(auth)/login/actions.ts
@@ -2,6 +2,7 @@
 
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
+import * as jose from "jose";
 import { AuthFormData } from "@repo/shared-lib/zod-schemas";
 import { ApiResponse } from "@repo/shared-lib/api-response";
 import { LoginUserResponse } from "@repo/shared-lib/api-response/users";
@@ -29,22 +30,19 @@ export const loginUser = async (
 
   const cookieStore = await cookies();
 
-  // FIXME: should be taken from JWT payload
-  const expires = new Date(Date.now() + 1000 * 60 * 60 * 24 * 7);
-
   cookieStore.set("accessToken", data.accessToken, {
     httpOnly: true,
     secure: true,
     path: "/",
     sameSite: "strict",
-    expires,
+    expires: new Date(jose.decodeJwt(data.accessToken).exp! * 1000),
   });
   cookieStore.set("refreshToken", data.refreshToken, {
     httpOnly: true,
     secure: true,
     path: "/",
     sameSite: "strict",
-    expires,
+    expires: new Date(jose.decodeJwt(data.refreshToken).exp! * 1000),
   });
 
   // Redirect to Home Page on successful login
