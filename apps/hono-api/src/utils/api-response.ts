4e8a7f44ad418d837579cb5dc7ad38dc5e2afcda
Mubashir Hassan <mhm13dev@gmail.com>
2025-01-25 18:49:59 +0500

    (feat/api): change enum to object as const

--- apps/hono-api/src/utils/api-response.ts
+++ apps/hono-api/src/utils/api-response.ts
@@ -1,14 +1,17 @@
-export enum ApiResponseCode {
-  ok = "ok",
-  path_not_found = "path_not_found",
-  internal_server_error = "internal_server_error",
-  resource_not_found = "resource_not_found",
-  access_token_required = "access_token_required",
-  refresh_token_required = "refresh_token_required",
-  unauthorized = "unauthorized",
-  conflict = "conflict",
-  forbidden = "forbidden",
-}
+export const ApiResponseCode = {
+  ok: "ok",
+  path_not_found: "path_not_found",
+  internal_server_error: "internal_server_error",
+  resource_not_found: "resource_not_found",
+  access_token_required: "access_token_required",
+  refresh_token_required: "refresh_token_required",
+  unauthorized: "unauthorized",
+  conflict: "conflict",
+  forbidden: "forbidden",
+} as const;
+
+export type ApiResponseCode =
+  (typeof ApiResponseCode)[keyof typeof ApiResponseCode];
 
 export class ApiResponse {
   response_code: ApiResponseCode;
