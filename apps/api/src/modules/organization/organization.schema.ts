08752afb1fb4be5ebe5ee208a7b973bda47d1104
Mubashir Hassan <mhm13dev@gmail.com>
2025-07-12 23:32:34 +0500

    feat: use slugify for slug

--- apps/api/src/modules/organization/organization.schema.ts
+++ apps/api/src/modules/organization/organization.schema.ts
@@ -1,6 +1,11 @@
 import { z } from "zod";
+import slugify from "slugify";
 
 export const createOrganizationSchema = z.object({
   name: z.string().trim().min(1),
-  slug: z.string().trim().min(1),
+  slug: z
+    .string()
+    .trim()
+    .min(1)
+    .transform((val) => slugify(val, { lower: true, strict: true })),
 });
